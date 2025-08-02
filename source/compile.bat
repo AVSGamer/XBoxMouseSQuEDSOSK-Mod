533c1ec12c01827ff023822723f12fe277fa2625
PapiRE <lorman.mamuyac@gmail.com>
2025-08-02 08:58:33 +0800

    compiledAgain

--- source/compile.bat
+++ source/compile.bat
@@ -1,4 +1,4 @@
 taskkill /im XBoxMouse.exe
-del "C:\Users\Lorman Mamuyac\Documents\AHK3rdPartyTools\Gamepad2MouseKeyboard\XBoxMouseSQuEDSOSK-Mod\XBoxMouse.exe"
-"C:\Users\Lorman Mamuyac\Downloads\Xbox360Chatpad\AutoHotkey104805\Compiler\Ahk2exe.exe" /in "C:\Users\Lorman Mamuyac\Documents\AHK3rdPartyTools\Gamepad2MouseKeyboard\XBoxMouseSQuEDSOSK-Mod\source\XBoxMouse.ahk" /icon "C:\Users\Lorman Mamuyac\Documents\AHK3rdPartyTools\Gamepad2MouseKeyboard\XBoxMouseSQuEDSOSK-Mod\source\controller.ico" /pass "CustomPassword" /NoDecompile
+del "C:\Users\Lorman Mamuyac\Documents\AHK3rdPartyTools\Gamepad2MouseKeyboard\XBoxMouseSQuEDSOSK-Mod-1\XBoxMouse.exe"
+"C:\Users\Lorman Mamuyac\Downloads\Xbox360Chatpad\AutoHotkey104805\Compiler\Ahk2exe.exe" /in "C:\Users\Lorman Mamuyac\Documents\AHK3rdPartyTools\Gamepad2MouseKeyboard\XBoxMouseSQuEDSOSK-Mod-1\source\XBoxMouse.ahk" /icon "C:\Users\Lorman Mamuyac\Documents\AHK3rdPartyTools\Gamepad2MouseKeyboard\XBoxMouseSQuEDSOSK-Mod-1\source\controller.ico" /pass "CustomPassword" /NoDecompile
 
